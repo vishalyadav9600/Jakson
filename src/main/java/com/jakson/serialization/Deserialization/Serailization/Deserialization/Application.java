deefa8f3d98d60ae6cb2c167999d169566d082f6
vishalyadav_9600 <vishalyadav9600@outlook.com>
2023-06-26 02:23:08 +0530

    some changes to given code

--- src/main/java/com/jakson/serialization/Deserialization/Serailization/Deserialization/Application.java
+++ src/main/java/com/jakson/serialization/Deserialization/Serailization/Deserialization/Application.java
@@ -1,5 +1,8 @@
 package com.jakson.serialization.Deserialization.Serailization.Deserialization;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,24 +11,26 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-        // Create a Car object
-        Car car = new Car("AUDI", "Q8");
+		Car car1 = new Car("Audi", "Q8");
+        Car car2 = new Car("BMW", "X5");
+        Car car3 = new Car("Mercedes", "C-Class");
 
-        // Serialize the Car object to JSON
-        String serializedData = SerializationHelper.serializeObject(car);
+        // Serialize the car objects to JSON
+        String serializedData = SerializationHelper.serializeObject(new Car[]{car1, car2, car3});
         System.out.println("Serialized JSON:\n" + serializedData);
 
         // Save the serialized JSON to a file
-        SerializationHelper.saveToFile(serializedData, "car.json");
+        SerializationHelper.saveToFile(serializedData, "carModels.json");
 
         // Read the serialized JSON from the file
-        String loadedData = SerializationHelper.loadFromFile("car.json");
+        String loadedData = SerializationHelper.loadFromFile("carModels.json");
 
-        // Deserialize the JSON back to a Car object
-        Car deserializedCar = SerializationHelper.deserializeObject(loadedData, Car.class);
-        System.out.println("\nDeserialized Car:\n" + deserializedCar);
-
+        // Deserialize the JSON back to an array of car objects
+        Car[] deserializedCars = SerializationHelper.deserializeObject(loadedData, Car[].class);
+        System.out.println("\nDeserialized Car Models:");
+        for (Car car : deserializedCars) {
+            System.out.println(car);
+        }
 	}
 
 }
